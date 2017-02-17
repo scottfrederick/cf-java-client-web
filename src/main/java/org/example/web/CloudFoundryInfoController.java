b109de4754086a93d0bf6d0b9ac487eee92b0bc6
Scott Frederick <scottyfred@gmail.com>
2017-02-17 15:28:32 -0600

    Add log messages before and after every API call.

--- src/main/java/org/example/web/CloudFoundryInfoController.java
+++ src/main/java/org/example/web/CloudFoundryInfoController.java
@@ -1,14 +1,21 @@
 package org.example.web;
 
 import org.cloudfoundry.client.lib.CloudFoundryClient;
+import org.cloudfoundry.client.lib.domain.CloudApplication;
+import org.cloudfoundry.client.lib.domain.CloudRoute;
+import org.cloudfoundry.client.lib.domain.CloudService;
 import org.example.model.CloudFoundryInfo;
 import org.example.model.CloudFoundryProperties;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
+import java.util.List;
+
 @Controller
 public class CloudFoundryInfoController {
+
+	private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(CloudFoundryInfoController.class);
 
 	private CloudFoundryProperties cloudFoundryProperties;
 	private final CloudFoundryClient cloudFoundryClient;
@@ -25,8 +32,27 @@ public class CloudFoundryInfoController {
 	}
 
 	private CloudFoundryInfo getCloudInfo(CloudFoundryClient client) {
-		return new CloudFoundryInfo(client.getServices(),
-				client.getRoutes(cloudFoundryProperties.getDomain()),
-				client.getApplications());
+		return new CloudFoundryInfo(getServices(client), getRoutes(client), getApps(client));
+	}
+
+	private List<CloudApplication> getApps(CloudFoundryClient client) {
+		log.info("Getting applications");
+		List<CloudApplication> applications = client.getApplications();
+		log.info("Done getting applications");
+		return applications;
+	}
+
+	private List<CloudRoute> getRoutes(CloudFoundryClient client) {
+		log.info("Getting routes");
+		List<CloudRoute> routes = client.getRoutes(cloudFoundryProperties.getDomain());
+		log.info("Done getting routes");
+		return routes;
+	}
+
+	private List<CloudService> getServices(CloudFoundryClient client) {
+		log.info("Getting services");
+		List<CloudService> services = client.getServices();
+		log.info("Done getting services");
+		return services;
 	}
 }
